915110f9dacabd2045f25cffcec182921f94ae1c
Mikolaj Lukasik <mlukasik@gmail.com>
2019-08-13 21:33:54 +0100

    Initial Commit

---
+++ LifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by Mikolaj Lukasik on 13/08/2019.
+//  Copyright © 2019 Mikolaj Lukasik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
